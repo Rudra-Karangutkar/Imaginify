80bc9f49ec2ca1b722d1bcdfbc57cda53cab49bd
Rudra-Karangutkar <rudrakarangutkar@gmail.com>
2025-01-14 17:30:54 +0530

    initial commit

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TransformationsPage = () => {
+  return (
+    <div>
+      TransformationsPage
+    </div>
+  )
+}
+
+export default TransformationsPage
+
+
+// localhost:3000/transformations
+// localhost:3000/transformations/[id]
